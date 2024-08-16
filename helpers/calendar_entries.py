9d34f112341105fae06494032dafded03a25309c
econdepe <econdepe@protonmail.com>
2024-08-16 14:35:36 +0200

    Write helper to extract new free slots and unify helpers into single file

---
+++ helpers/calendar_entries.py
@@ -0,0 +1,103 @@
+import sqlite3
+
+from selenium.webdriver.common.by import By
+
+from .. import constants
+
+
+
+def extract_calendar_entries(browser):
+    table_body = browser.find_element(By.CLASS_NAME, 'fc-body')
+    week_dates = [table_body.find_element(By.CLASS_NAME, f"fc-{day}").get_attribute('data-date') for day in ['mon', 'tue', 'wed', 'thu', 'fri']]
+    grid = table_body.find_element(By.CLASS_NAME, 'fc-content-skeleton')
+    rows = [row.text.split('\n') for row in grid.find_elements(By.TAG_NAME, 'tr')]
+
+    '''
+        We store the calendar entries in a dictionary { [key]: value }, where:
+            * key: str
+                Date + time of the session in ISO format. E.g.: '2024-08-12T18:00:00'
+            * value: str.
+                Names of the participants of the session, separated by commas.
+                If a session has been cancelled, the name used is 'CANCELLED'
+                E.g.: 'Bond J.,Norbert E.,CANCELLED,CANCELLED'
+    '''
+    calendar_entries = {}
+
+
+    for row in rows:
+        for i, entry in enumerate(row):
+            # Entries have the format '18:00 Bond J.'
+            time = entry[:5]
+            name = entry[6:]
+            if name == 'Session annulée':
+                name = constants.CANCELLED
+            elif name == 'Place disponible':
+                name = constants.FREE
+            date = week_dates[i]
+            datetime = f"{date}T{time}:00"
+            if datetime in calendar_entries:
+                calendar_entries[datetime] += f",{name}"
+            else:
+                calendar_entries[datetime] = name
+
+def write_calendar_entries_to_db(calendar_entries):
+    conn = sqlite3.connect(f"wake_alert.db")
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS calendar(
+            datetime TEXT
+            , members TEXT
+        )
+    ''')
+
+    for datetime in calendar_entries:
+        cursor.execute('''
+            SELECT members
+            FROM calendar
+            WHERE datetime = ?
+        ''', (datetime,))
+        old_members_array = cursor.fetchall()
+
+        if len(old_members_array) == 0:
+            # No previous entry exists
+            cursor.execute('''
+                INSERT INTO calendar (datetime, members)
+                VALUES (?, ?)
+            ''', (datetime, calendar_entries[datetime]))
+        else:
+            # Overwrite previous entry
+            cursor.execute('''
+                UPDATE calendar
+                SET members = ?
+                WHERE datetime = ?
+            ''', (calendar_entries[datetime], datetime))
+
+    conn.commit()
+    cursor.close()
+
+def count_slots_available(new_members: str, old_members: str) -> int:
+    new_free_slots = new_members.count(constants.FREE)
+    old_free_slots = old_members.count(constants.FREE)
+
+    return max(new_free_slots - old_free_slots, 0)
+
+
+def find_available_slots(calendar_entries):
+    conn = sqlite3.connect(f"wake_alert.db")
+    cursor = conn.cursor()
+
+    result = {}
+
+    for datetime in calendar_entries:
+        cursor.execute('''
+            SELECT members
+            FROM calendar
+            WHERE datetime = ?
+        ''', (datetime,))
+        old_members = cursor.fetchone()[0]
+
+        if (count := count_slots_available(calendar_entries[datetime], old_members)):
+            result[datetime] = count
+
+    cursor.close()
