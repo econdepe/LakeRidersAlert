f061252b3bd3b581af866a978707490d2e135803
econdepe <econdepe@protonmail.com>
2024-09-01 13:57:45 +0200

    Crawl using simple HTTP requests

--- wakealert/helpers/run.py
+++ wakealert/helpers/run.py
@@ -3,10 +3,9 @@ from time import sleep
 from zoneinfo import ZoneInfo
 
 from ..constants import POLLING_INTERVAL, RUN_WITH_LOGS
-from ..helpers.browser import (
-    create_browser,
-    navigate_to_calendar,
-    navigate_to_next_week,
+from ..helpers.web import (
+    create_browser_session,
+    get_reservations_html,
 )
 from ..helpers.calendar_entries import (
     extract_calendar_entries,
@@ -16,23 +15,17 @@ from ..helpers.calendar_entries import (
 from ..helpers.telegram_bot import notify_to_telegram
 
 
-def run_once(browser=None):
+def run_once(session=None):
     if RUN_WITH_LOGS:
         message = (
             f"Crawling lakeriders calendar on {datetime.now().strftime('%d %b, %H:%M')}"
         )
         print(f"{'-'*len(message)}\n{message}\n...\n..\n.")
 
-    browser_session = create_browser() if browser is None else browser
-    navigate_to_calendar(browser_session)
+    browser_session = create_browser_session() if session is None else session
+    html = get_reservations_html(browser_session)
 
-    calendar_entries_current_week = extract_calendar_entries(browser_session)
-    navigate_to_next_week(browser_session)
-    calendar_entries_next_week = extract_calendar_entries(browser_session)
-    calendar_entries = {**calendar_entries_current_week, **calendar_entries_next_week}
-    if RUN_WITH_LOGS:
-        print("* * Crawling result:")
-        print(calendar_entries)
+    calendar_entries = extract_calendar_entries(html)
     available_slots = find_available_slots(calendar_entries)
     if available_slots is not None:
         notify_to_telegram(available_slots)
---
+++ wakealert/helpers/web.py
@@ -0,0 +1,38 @@
+from requests import Session
+
+from ..constants import LOGIN_EMAIL, LOGIN_PASSWORD, RUN_WITH_LOGS
+
+HOME_PAGE = "https://lakeridersclub.ch/index.php"
+AUTHENTICATION_PAGE = "https://lakeridersclub.ch/membres/connexion.php"
+CALENDAR_PAGE = "https://lakeridersclub.ch/membres/reservations.php"
+
+
+def create_browser_session():
+    session = Session()
+    session.get(HOME_PAGE)
+
+
+def get_session_authorized(session):
+    if RUN_WITH_LOGS:
+        print("Authenticating")
+
+    session.post(
+        url=AUTHENTICATION_PAGE,
+        data={
+            "adresse_electronique": LOGIN_EMAIL,
+            "mot_de_passe": LOGIN_PASSWORD,
+            "rester_connecte": 1,
+            "action": "se_connecter",
+        },
+    )
+
+
+def get_reservations_html(session):
+    response = session.get(CALENDAR_PAGE)
+    if response.url == HOME_PAGE:
+        # The session is not authenticated. Re-authenticate
+        get_session_authorized(session)
+        authenticated_response = session.get(CALENDAR_PAGE)
+        return authenticated_response.text
+    else:
+        return response.text
